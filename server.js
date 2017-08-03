c7d8bd860967a23d96b6434970e7bdf716004727
nikrb <nik_rb@yahoo.com>
2017-08-03 22:15:51 +0100

    add file upload and return file size

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-
+const fs = require('fs');
 const express = require('express');
+var multer = require( 'multer');
+var upload = multer({ dest: 'uploads/'});
 const app = express();
 
 app.set('port', (process.env.PORT || 8080));
@@ -7,6 +9,13 @@ app.use('/', express.static(process.cwd() + '/public'));
 
 app.get( '/', (req,res) => {
   res.sendFile( process.cwd() + '/public/index.html');
+});
+
+app.post( '/upload', upload.single( 'myfile'), function( req, res){
+  fs.stat( req.file.path, (err, stats) => {
+    const result = { filesize: stats.size};
+    res.send( {result: result});
+  });
 });
 
 app.listen(app.get('port'), () => {
